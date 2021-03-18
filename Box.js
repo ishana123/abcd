de140b7d7381bfe23048785388204d4b33773226
ishana123 <77000627+ishana123@users.noreply.github.com>
2021-03-18 12:58:33 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,23 @@
+class Box{
+    constructor(x , y ,width , height){
+        var options = {
+            restitution : 1 
+        }
+       this.body = Bodies.rectangle(x , y , width , height , options);
+       this.width = width ;
+       this.height  = height;
+       World.add(world,this.body);
+    }
+    display(){
+        var p = this.body.position;
+        var angle = this.body.angle;
+         push()
+         translate(p.x,p.y);
+         rotate(angle);
+
+        rectMode (CENTER);
+    
+        rect(0 , 0, this.width , this.height);
+        pop()
+    }
+}
